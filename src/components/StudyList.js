c796ee2ade06b0b00315b10cb22a939f8ff021ca
Joyce Woo <yyjoycewoo@gmail.com>
2018-05-31 11:14:34 -0400

    Add buttons to toggle between modes

--- src/components/StudyList.js
+++ src/components/StudyList.js
@@ -5,19 +5,25 @@ import AddItem from "./AddItem";
 
 class StudyList extends Component {
   render() {
-    return this.renderCards();
+    if (this.props.mode == "study") {
+      return this.renderStudyList();
+    } else if (this.props.mode == "test") {
+      return this.renderTestList();
+    }
   }
 
   constructor(props) {
     super(props);
     this.state = { currentCard: 0 };
-    this.renderCards = this.renderCards.bind(this);
+    this.renderStudyList = this.renderStudyList.bind(this);
+    this.renderTestList = this.renderTestList.bind(this);
     this.updateCard = this.updateCard.bind(this);
   }
 
-  renderCards() {
-    const card = this.props.cards[this.state.currentCard];
-    console.log(card);
+  renderStudyList() {
+    const currCard = this.state.currentCard;
+    const card = this.props.cards[currCard];
+    const numCards = this.props.cards.length;
     return (
       <div className="StudyList">
         <Flashcard key={card["key"]} 
@@ -27,6 +33,25 @@ class StudyList extends Component {
           answer={card["answer"]} 
           display={this.props.mode} />
         <button onClick={this.updateCard}>Next</button>
+        <div>Progress: {currCard+1} / {numCards}</div>
+      </div>
+    )
+  }
+
+  renderTestList() {
+    const currCard = this.state.currentCard;
+    const card = this.props.cards[currCard];
+    const numCards = this.props.cards.length;
+    return (
+      <div className="StudyList">
+        <Flashcard key={card["key"]} 
+          id={card["key"]}
+          setId={card["setId"]}
+          question={card["question"]} 
+          answer={card["answer"]} 
+          display={this.props.mode} />
+        <button onClick={this.updateCard}>Next</button>
+        <div>Accuracy: {currCard+1} / {numCards}</div>
       </div>
     )
   }
