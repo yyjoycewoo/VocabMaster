de60bea99f1b52328a26ac98a9747df6560bc0ba
Joyce Woo <yyjoycewoo@gmail.com>
2018-05-31 11:20:22 -0400

    refactor code

--- src/components/StudyList.js
+++ src/components/StudyList.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "../styles/FlashcardList.css";
 import Flashcard from "./Flashcard";
-import AddItem from "./AddItem";
 
 class StudyList extends Component {
   render() {
