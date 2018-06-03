2f57f3a149dca384523e1577cda76ea209e54e86
Danny Liu <danny.liu@flipp.com>
2018-06-03 11:14:30 -0400

    setting default mode to browse

--- src/components/View.js
+++ src/components/View.js
@@ -16,7 +16,7 @@ class View extends Component {
     this.state = {
       sets: sets,
       currentSet: currSet,
-      mode: 'study',
+      mode: 'browse',
     }
 
     this.updateCurrentSet = this.updateCurrentSet.bind(this);
